1db0a0466d7d6fec1913592f707dd4e636116eca
= <=>
2021-07-01 20:31:24 +0100

    admin orders first commit

--- src/pages/adminorders/AdminOrders.js
+++ src/pages/adminorders/AdminOrders.js
@@ -1,16 +1,61 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import './AdminOrders.css';
 import AdminLayout from "../../components/adminlayout/AdminLayout";
+import {Table} from '../../components/table/Table';
 
 const AdminOrders = (props) => {
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: 'Ordered By',
+        columns: [
+          {
+            Header: 'First Name',
+            accessor: 'firstName',
+          },
+          {
+            Header: 'Last Name',
+            accessor: 'lastName',
+          },
+          {
+            Header: 'Email',
+            accessor: 'email',
+          },
+        ],
+      },
+      {
+        Header: 'Info',
+        columns: [
+          {
+            Header: 'Quantity',
+            accessor: 'quantity',
+          },
+          {
+            Header: 'Total Cost',
+            accessor: 'total cost',
+          },
+          {
+            Header: 'Delivery Method',
+            accessor: 'delivery method',
+          },
+          {
+            Header: 'Status',
+            accessor: 'status',
+          },
+        ],
+      },
+    ],
+    []
+  )
+
+  const data = [];
+
   return (
      <AdminLayout>
-       <div className="orders-wrapper">
-        <div className="title">Orders</div>
-         <div className="orders-list-wrapper">
-
-         </div>
-       </div>
+      <div className="orders-wrapper">
+        <Table columns={columns} data={data}/>
+      </div>
      </AdminLayout>
   );
 }
