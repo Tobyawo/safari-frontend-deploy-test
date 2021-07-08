0651eac425060f0cf5f67847f0960b2e93df0c36
Beyond-Logic <edemaero@gmail.com>
2021-07-08 23:56:56 +0100

    ProductApi Update

--- src/apis/ProductApi.js
+++ src/apis/ProductApi.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import setHeader from "../utilities/Header";
 
 const token = localStorage.getItem("token");
+console.log(token);
 
 const productApis = {
   getAllProduct: async () => {
@@ -17,11 +18,14 @@ const productApis = {
     return products;
   },
 
+  getProductById: async (id) => {
+    const { data: product } = await axios.get(`${BaseUrl}/products/${id}`);
+
+    return product;
+  },
+
   getAllProducts: async () => {
-    const { data: products } = await axios.get(
-      `${BaseUrl}/products`,
-      setHeader()
-    );
+    const { data: products } = await axios.get(`${BaseUrl}/products`);
 
     return products;
   },
