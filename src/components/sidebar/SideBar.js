580a11e93372ff0e744498c5429b91564a1501c0
Tobyawo <47924576+Tobyawo@users.noreply.github.com>
2021-07-09 16:38:51 +0100

    added responsiveness to user dashboard

--- src/components/sidebar/SideBar.js
+++ src/components/sidebar/SideBar.js
@@ -1,39 +1,40 @@
-import React from 'react';
-import {NavLink, withRouter} from 'react-router-dom';
-import './SideBar.css';
-import '../../styles/Components/_button.scss'
-
-const SideBar = ({menuItems}) => {
-
+import React from "react";
+import { NavLink, withRouter } from "react-router-dom";
+import "./SideBar.css";
+import "../../styles/Components/_button.scss";
+const SideBar = ({ menuItems }) => {
   return (
-    <div>
-    
-     <div className="sidebar-wrapper">
-     <h1 style={{textAlign:'center', marginTop:'20px'}}>Dashboard</h1>
-       <div className="menu-wrapper">
-         { 
-           menuItems.map((menuItem, index) => (   
-              <NavLink
-                 className={`nav-link`}
-               to={menuItem.path}
-                 key={index}
-                 exact={menuItem.exact}
-                 activeStyle={{
-                   color: '#FFFFFF',
-                   background: '#ED165F',
-                   borderRadius: '0 50px 50px 0',
-                 }}
-              >
-                <i className={`${menuItem.icon}`}></i>
-                {menuItem.name}
-              </NavLink>
-           ))
-         }
-       </div>
-       <button className="signout-button"><img src="../images/sign-out-icon.svg" alt="signout-icon" className="signout-image"></img>Sign Out </button>
-     </div>
+    // <div>
+    <div className="sidebar-wrapper">
+      <h1 className="title" style={{ textAlign: "center", marginTop: "20px" }}>
+        Dashboard
+      </h1>
+      <div className="menu-wrapper">
+        {menuItems.map((menuItem, index) => (
+          <NavLink
+            className={`nav-link`}
+            to={menuItem.path}
+            key={index}
+            exact={menuItem.exact}
+            activeStyle={{
+              color: "#FFFFFF",
+              background: "#ED165F",
+              borderRadius: "0 50px 50px 0",
+            }}
+          >
+            <i className={`${menuItem.icon} nav-link-icon`}></i>
+            <span className="nav-link-text">{menuItem.name}</span>
+          </NavLink>
+        ))}
       </div>
+      <button className="sign-out-button">
+        <span className="sign-out-icon">
+          <img src="../images/sign-out-icon.svg" alt="sign-out-icon" />
+        </span>
+        <span className="sign-out-text">Sign Out </span>
+      </button>
+    </div>
+    //   </div>
   );
-}
-
+};
 export default withRouter(SideBar);
